04ba7605d28bacf7ed88c213a41a2325a3cbd3f0
anubhav <anubhavdxt46@gmail.com>
2024-03-18 19:50:48 +0530

    Know your mushroom view update (#1)
    * refactor: Moved Home to screens directory and removed pages directory
    
    * feat: Add navigation and screens to the app
    
    - Added navigation using React Navigation to navigate between Home and Know Your Mushroom screens.
    - Updated the App component to include NavigationContainer and Stack Navigator for the screens.
    - Imported and included the KnowYourMushroom screen in the navigation stack.
    - Updated the title for the screens in the navigation options.

--- src/screens/KnowYourMushroom.tsx
+++ src/screens/KnowYourMushroom.tsx
@@ -1,18 +1,21 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const Home = () => {
+const KnowYourMushroom = () => {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text>KnowYourMushroom</Text>
     </View>
   );
 };
 
-export default Home;
+export default KnowYourMushroom;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
