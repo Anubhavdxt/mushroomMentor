71625ccbf06cf2769825ecf4fa6d40cef8b7d5e2
Anubhavdxt <anubhavdxt46@gmail.com>
2024-03-18 19:49:38 +0530

    feat: Add navigation and screens to the app
    - Added navigation using React Navigation to navigate between Home and Know Your Mushroom screens.
    - Updated the App component to include NavigationContainer and Stack Navigator for the screens.
    - Imported and included the KnowYourMushroom screen in the navigation stack.
    - Updated the title for the screens in the navigation options.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const App = () => {
+// Navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// Screens
+import Home from './screens/Home';
+import KnowYourMushroom from './screens/KnowYourMushroom';
+
+export type RootStackParamList = {
+  Home: undefined;
+  KnowYourMushroom: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const App = (): JSX.Element => {
   return (
-    <View style={styles.container}>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Home',
+          }}
+        />
+        <Stack.Screen
+          name="KnowYourMushroom"
+          component={KnowYourMushroom}
+          options={{
+            title: 'Know Your Mushroom',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
