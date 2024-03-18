9dd0b50a9a39504abc6ce4b2b30c03552f7e5f7e
Anubhav <anubhavdxt46@gmail.com>
2024-03-18 21:09:15 +0530

    ✨ setup:  Add new colors, sizes, and shadows
    ℹ️ Add new color constants, size constants, and shadow configurations for styling purposes.

---
+++ src/constants/icons.ts
@@ -0,0 +1,21 @@
+import logo from '../assets/icons/icon.png';
+import filter from '../assets/icons/filter.png';
+import search from '../assets/icons/search.png';
+import menu from '../assets/icons/menu.png';
+import heart from '../assets/icons/heart.png';
+import heartOl from '../assets/icons/heart-ol.png';
+import left from '../assets/icons/left.png';
+import share from '../assets/icons/share.png';
+import close from '../assets/icons/close.png';
+
+export default {
+  logo,
+  filter,
+  search,
+  menu,
+  heart,
+  heartOl,
+  left,
+  share,
+  close,
+};
---
+++ src/constants/images.ts
@@ -0,0 +1,25 @@
+import logo from '../assets/images/logo.png';
+
+import BlackOyster from '../assets/images/mushrooms/blackOyster.jpg';
+import BlueOyster from '../assets/images/mushrooms/blueOyster.jpg';
+import ElmOyster from '../assets/images/mushrooms/elmOyster.jpg';
+import GoldenOyster from '../assets/images/mushrooms/goldenOyster.jpg';
+import GreyOyster from '../assets/images/mushrooms/greyOyster.jpg';
+import KingOyster from '../assets/images/mushrooms/kingOyster.jpg';
+import PearlOyster from '../assets/images/mushrooms/pearlOyster.jpg';
+import PhoenixOyster from '../assets/images/mushrooms/phoenixOyster.jpg';
+import PinkOyster from '../assets/images/mushrooms/pinkOyster.png';
+
+export default {
+  logo,
+
+  BlackOyster,
+  BlueOyster,
+  ElmOyster,
+  GoldenOyster,
+  GreyOyster,
+  KingOyster,
+  PearlOyster,
+  PhoenixOyster,
+  PinkOyster,
+};
