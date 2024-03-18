8a605d7fde515cd2d6d9d21c5af8f182923b79f6
Anubhavdxt <anubhavdxt46@gmail.com>
2024-03-18 19:48:46 +0530

    refactor: Moved Home to screens directory and removed pages directory

---
+++ src/screens/Home.tsx
@@ -0,0 +1,24 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../App';
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const Home = ({navigation}: HomeProps): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
